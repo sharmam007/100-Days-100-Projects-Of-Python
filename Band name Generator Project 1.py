8c74e0e81c4a5209c6098c0a79a29946dead1e76
Mohit sharma <138148495+sharmam007@users.noreply.github.com>
2024-09-09 10:17:18 +0530

    Add files via upload

---
+++ Band name Generator Project 1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Welcome to band Name Generator")
+city = input("Enter the city name in which you grown up \n")
+pet = input("Enter the name of your pet \n")
+print("Your Band Name could be: "+city+" "+pet)
+
+
+# In[ ]:
+
+
+
+
