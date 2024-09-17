1094995f911d2bbc720dfa9280938533cc855c38
Mohit sharma <138148495+sharmam007@users.noreply.github.com>
2024-09-17 12:19:20 +0530

    Add files via upload

---
+++ Day 9 Project 9 Blind Auction Project.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import art
+print(art.logo)
+def find_highest_bidder(bidding_dictionary):
+    winner = ""
+    highest_bid = 0
+    max(bidding_dictionary)
+    for bidder in bidding_dictionary:
+        bid_amount = bidding_dictionary[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"the winner is {winner} with a bid amount of ${highest_bid}")
+
+
+bids = {}
+continue_bidding = True
+while continue_bidding:
+    name = str(input("Enter your name; \n"))
+    price = int(input("what is your bid \n $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no'.\n ").lower()
+    if should_continue == "no":
+        continue_bidding = False
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        print("\n"*20)
+
+
+
